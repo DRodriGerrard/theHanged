6fd428743e1c826067ce9f5a9ccdafa27f871be5
David Rodri <drodrigerrard@gmail.com>
2020-10-10 16:11:30 +0100

    generate random word

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'theHanged';
+
+  private gameStructure:any = {
+    rules: {
+      time: 180,
+      life: 6,
+      clue: 1
+    },
+    words: [
+      "palanca",
+      "la bomba"
+    ]
+  }
+  randomWord:number = 0;
+  wordLetters:string[] = [];
+  time:number = this.gameStructure.rules.time;
+
+
+  start() {
+    setInterval(() => {
+      if(this.time > 0) this.time += -1;
+    }, 1000);
+
+    setTimeout(() => {
+      this.generateRandomWord();
+    }, 0);
+  }
+
+  generateRandomWord() {
+    this.randomWord = Math.floor(Math.random() * this.gameStructure.words.length);
+    this.wordLetters = this.gameStructure.words[this.randomWord].split("");
+  }
 }
